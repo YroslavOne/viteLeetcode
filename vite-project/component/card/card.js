852023800d6153bc10415372140f5240413e5f91
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-11-14 22:24:10 +0700

    1

--- vite-project/component/card/card.js
+++ vite-project/component/card/card.js
@@ -1,58 +1,53 @@
-import cardProduct from "./cardProduct";
+import cardProduct from './cardProduct';
 
-function card(elemHtml){
-const $busketOpen = document.createElement("div")
-$busketOpen.id = "basket_open"
-elemHtml.append($busketOpen);
+function card(elemHtml) {
+  const $busketOpen = document.createElement('div');
+  $busketOpen.id = 'basket_open';
+  elemHtml.append($busketOpen);
 
-const $blockBasket = document.createElement("div")
-$blockBasket.className = "block_basket"
-$busketOpen.append($blockBasket);
-const $closeBasket = document.createElement("div");
-$closeBasket.className = "close_basket";
-$closeBasket.id = "toDoBasketClose";
-$blockBasket.append($closeBasket);
-const $iBasket = document.createElement("i");
-$iBasket.className = "bi bi-x";
-$closeBasket.append($iBasket);
-const $basketBlockMain = document.createElement("div");
-$basketBlockMain.className = "basket_block_main"
-$blockBasket.append($basketBlockMain);
-const $h3TitleBascet = document.createElement("h3");
-$h3TitleBascet.className = "title_basket";
-$h3TitleBascet.textContent = "Your Bag";
-$basketBlockMain.append($h3TitleBascet);
-const $blockBasketProduct = document.createElement("div");
-$blockBasketProduct.className = "block_basket_product";
-$blockBasketProduct.id = "block_basket_product";
-$basketBlockMain.append($blockBasketProduct);
-const $elemBlockBasketProduct = document.getElementById("block_basket_product");
-$blockBasketProduct.append(cardProduct());
+  const $blockBasket = document.createElement('div');
+  $blockBasket.className = 'block_basket';
+  $busketOpen.append($blockBasket);
+  const $closeBasket = document.createElement('div');
+  $closeBasket.className = 'close_basket';
+  $closeBasket.id = 'toDoBasketClose';
+  $blockBasket.append($closeBasket);
+  const $iBasket = document.createElement('i');
+  $iBasket.className = 'bi bi-x';
+  $closeBasket.append($iBasket);
+  const $basketBlockMain = document.createElement('div');
+  $basketBlockMain.className = 'basket_block_main';
+  $blockBasket.append($basketBlockMain);
+  const $h3TitleBascet = document.createElement('h3');
+  $h3TitleBascet.className = 'title_basket';
+  $h3TitleBascet.textContent = 'Your Bag';
+  $basketBlockMain.append($h3TitleBascet);
+  const $blockBasketProduct = document.createElement('div');
+  $blockBasketProduct.className = 'block_basket_product';
+  $blockBasketProduct.id = 'block_basket_product';
+  $basketBlockMain.append($blockBasketProduct);
+  const $elemBlockBasketProduct = document.getElementById(
+    'block_basket_product'
+  );
+  $blockBasketProduct.append(cardProduct());
 
+  const $divPraiseBasket = document.createElement('div');
+  $divPraiseBasket.className = 'div_prise_basket';
+  $basketBlockMain.append($divPraiseBasket);
+  const $pPraiseBasket = document.createElement('p');
+  $pPraiseBasket.className = '_prise_basket';
+  $pPraiseBasket.textContent = `Total : $сумма`;
+  $divPraiseBasket.append($pPraiseBasket);
 
+  const $buttonBasketChekout = document.createElement('button');
+  $buttonBasketChekout.className = 'basket_checkout';
+  $buttonBasketChekout.textContent = 'checkout';
+  $basketBlockMain.append($buttonBasketChekout);
 
-
-const $divPraiseBasket = document.createElement("div")
-$divPraiseBasket.className = "div_prise_basket";
-$basketBlockMain.append($divPraiseBasket);
-const $pPraiseBasket = document.createElement("p")
-$pPraiseBasket.className = "_prise_basket";
-$pPraiseBasket.textContent = `Total : $сумма`;
-$divPraiseBasket.append($pPraiseBasket);
-
-
-
-const $buttonBasketChekout = document.createElement("button")
-$buttonBasketChekout.className = "basket_checkout"
-$buttonBasketChekout.textContent = "checkout"
-$basketBlockMain.append($buttonBasketChekout);
-
-
-
-
-const $clickElemCloseBasket = document.getElementById("toDoBasketClose")
-$clickElemCloseBasket.addEventListener("click", ()=> {console.log("closeBasket")})
-
+  const $clickElemCloseBasket = document.getElementById('toDoBasketClose');
+  $clickElemCloseBasket.addEventListener('click', () => {
+    console.log('closeBasket');
+  });
 }
 
 export default card;
--- vite-project/component/card/cardProduct.js
+++ vite-project/component/card/cardProduct.js
@@ -1,54 +1,69 @@
-function cardProduct(elemHtml){
-    const $divDisplayFlexBasketProduct = document.createElement("div")
-    $divDisplayFlexBasketProduct.className = "display_flex_basket_product";
-    elemHtml.appendChild($divDisplayFlexBasketProduct);
-    const $divImgElemBasketProduct = document.createElement("div");
-    $divImgElemBasketProduct.className = "img_elem_basket_product"
-    $divDisplayFlexBasketProduct.appendChild($divImgElemBasketProduct);
-    const $imgElemBusketProduct = document.createElement("img");
-    $imgElemBusketProduct.src = "https://storage.mds.yandex.net/get-canvas-html5/1003119/8cf9e790-4057-43c2-8d7b-0baf6acddf3b/image.jpg"
-    $divImgElemBasketProduct.appendChild($imgElemBusketProduct);
-    const $divTextElemBasketProduct = document.createElement("div");
-    $divTextElemBasketProduct.className = "text_elem_basket_product";
-    $divDisplayFlexBasketProduct.appendChild($divTextElemBasketProduct);
-    const $pTitleElemBasketProduct = document.createElement("p")
-    $pTitleElemBasketProduct.className = "title_elem_basket_product"
-    $divTextElemBasketProduct.appendChild($pTitleElemBasketProduct);
-    const $pPriceElemBasketProduct = document.createElement("p")
-    $pPriceElemBasketProduct.className = "price_elem_basket_product"
-    $divTextElemBasketProduct.appendChild($pPriceElemBasketProduct);
-    const $buttonElemBasketProduct = document.createElement("button")
-    $buttonElemBasketProduct.className = "button_elem_basket_product";
-    $buttonElemBasketProduct.id = "removeItemBasket";
-    $buttonElemBasketProduct.textContent = "remove";
-    $divTextElemBasketProduct.appendChild($buttonElemBasketProduct);
-    const $divQuantityElemBasketProduct = document.createElement("div");
-    $divQuantityElemBasketProduct.className = "quantity_elem_basket_product";
-    $divDisplayFlexBasketProduct.appendChild($divQuantityElemBasketProduct);
-    const $buttonQuantityPlus = document.createElement("button");
-    $buttonQuantityPlus.className = "quantity_plus";
-    $buttonQuantityPlus.id = "productPlus"
-    const $pQuantityTextBasketProduct = document.createElement('p');
-    const $buttonQuantityMinus = document.createElement("button")
-    $buttonQuantityMinus.className = "quantity_minus";
-    $buttonQuantityMinus.id = "productMinus"
+function cardProduct(elemHtml) {
+  const $divDisplayFlexBasketProduct = document.createElement('div');
+  $divDisplayFlexBasketProduct.className = 'display_flex_basket_product';
+  elemHtml.appendChild($divDisplayFlexBasketProduct);
+  const $divImgElemBasketProduct = document.createElement('div');
+  $divImgElemBasketProduct.className = 'img_elem_basket_product';
+  $divDisplayFlexBasketProduct.appendChild($divImgElemBasketProduct);
+  const $imgElemBasketProduct = document.createElement('img');
+  $imgElemBasketProduct.src =
+    'https://storage.mds.yandex.net/get-canvas-html5/1003119/8cf9e790-4057-43c2-8d7b-0baf6acddf3b/image.jpg';
+  $divImgElemBasketProduct.appendChild($imgElemBasketProduct);
+  const $divTextElemBasketProduct = document.createElement('div');
+  $divTextElemBasketProduct.className = 'text_elem_basket_product';
+  $divDisplayFlexBasketProduct.appendChild($divTextElemBasketProduct);
+  const $pTitleElemBasketProduct = document.createElement('p');
+  $pTitleElemBasketProduct.className = 'title_elem_basket_product';
+  $divTextElemBasketProduct.appendChild($pTitleElemBasketProduct);
+  const $pPriceElemBasketProduct = document.createElement('p');
+  $pPriceElemBasketProduct.className = 'price_elem_basket_product';
+  $divTextElemBasketProduct.appendChild($pPriceElemBasketProduct);
+  const $buttonElemBasketProduct = document.createElement('button');
+  $buttonElemBasketProduct.className = 'button_elem_basket_product';
+  $buttonElemBasketProduct.id = 'removeItemBasket';
+  $buttonElemBasketProduct.textContent = 'remove';
+  $divTextElemBasketProduct.appendChild($buttonElemBasketProduct);
+  const $divQuantityElemBasketProduct = document.createElement('div');
+  $divQuantityElemBasketProduct.className = 'quantity_elem_basket_product';
+  $divDisplayFlexBasketProduct.appendChild($divQuantityElemBasketProduct);
+  const $buttonQuantityPlus = document.createElement('button');
+  $buttonQuantityPlus.className = 'quantity_plus';
+  $buttonQuantityPlus.id = 'productPlus';
+  $divQuantityElemBasketProduct.appendChild($buttonQuantityPlus);
+  const $iProductPlus = document.createElement('i');
+  $iProductPlus.className = 'bi bi-caret-up';
+  $iProductPlus.id = 'productPlus';
 
+  $buttonQuantityPlus.appendChild($iProductPlus);
 
-    // <div class="display_flex_basket_product">
-    // <div class="img_elem_basket_product">
-    //     <img src="${element.img}" alt="">
-    // </div>
-    // <div class="text_elem_basket_product">
-        // <p class="title_elem_basket_product">${element.name}</p>
-        // <p class="price_elem_basket_product">${element.price}</p>
-        // <button class="button_elem_basket_product" onclick="removeItemBasket(${indexObjProduct})">remove</button>
-    
-    // </div>
-    // <div class="quantity_elem_basket_product">
-        <button class="quantity_plus" onclick=productPlus()><i class="bi bi-caret-up"></i></button>
-        <p class="quantity_text_basket_product">${element.quantity}</p>
-        <button class="quantity_minus" onclick=productMinus()><i class="bi bi-caret-down"></i></button>
-    // </div>
-    // </div>
+  const $pQuantityTextBasketProduct = document.createElement('p');
+  $pQuantityTextBasketProduct.className = 'quantity_text_basket_product';
+  $divQuantityElemBasketProduct.appendChild($pQuantityTextBasketProduct);
+
+  const $buttonQuantityMinus = document.createElement('button');
+  $buttonQuantityMinus.className = 'quantity_minus';
+  $buttonQuantityMinus.id = 'productMinus';
+  $divQuantityElemBasketProduct.appendChild($buttonQuantityMinus);
+  const $iProductMinus = document.createElement('i');
+  $iProductMinus.className = 'bi bi-caret-down';
+  $iProductMinus.id = 'productMinus';
+  $buttonQuantityMinus.appendChild($iProductMinus);
+
+  // <div class="display_flex_basket_product">
+  // <div class="img_elem_basket_product">
+  //     <img src="${element.img}" alt="">
+  // </div>
+  // <div class="text_elem_basket_product">
+  // <p class="title_elem_basket_product">${element.name}</p>
+  // <p class="price_elem_basket_product">${element.price}</p>
+  // <button class="button_elem_basket_product" onclick="removeItemBasket(${indexObjProduct})">remove</button>
+
+  // </div>
+  // <div class="quantity_elem_basket_product">
+  // <button class="quantity_plus" onclick=productPlus()><i class="bi bi-caret-up"></i></button>
+  // <p class="quantity_text_basket_product">${element.quantity}</p>
+  // <button class="quantity_minus" onclick=productMinus()><i class="bi bi-caret-down"></i></button>
+  // </div>
+  // </div>
 }
 export default cardProduct;
