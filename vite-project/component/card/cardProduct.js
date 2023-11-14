682cd04275c348d876408e2bd3a74e8fb5dde245
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-11-14 17:47:24 +0700

    1

--- vite-project/component/card/cardProduct.js
+++ vite-project/component/card/cardProduct.js
@@ -22,7 +22,16 @@ function cardProduct(elemHtml){
     $buttonElemBasketProduct.id = "removeItemBasket";
     $buttonElemBasketProduct.textContent = "remove";
     $divTextElemBasketProduct.appendChild($buttonElemBasketProduct);
-    const $divQuantityElem_basket_product
+    const $divQuantityElemBasketProduct = document.createElement("div");
+    $divQuantityElemBasketProduct.className = "quantity_elem_basket_product";
+    $divDisplayFlexBasketProduct.appendChild($divQuantityElemBasketProduct);
+    const $buttonQuantityPlus = document.createElement("button");
+    $buttonQuantityPlus.className = "quantity_plus";
+    $buttonQuantityPlus.id = "productPlus"
+    const $pQuantityTextBasketProduct = document.createElement('p');
+    const $buttonQuantityMinus = document.createElement("button")
+    $buttonQuantityMinus.className = "quantity_minus";
+    $buttonQuantityMinus.id = "productMinus"
 
 
     // <div class="display_flex_basket_product">
@@ -35,11 +44,11 @@ function cardProduct(elemHtml){
         // <button class="button_elem_basket_product" onclick="removeItemBasket(${indexObjProduct})">remove</button>
     
     // </div>
-    <div class="quantity_elem_basket_product">
+    // <div class="quantity_elem_basket_product">
         <button class="quantity_plus" onclick=productPlus()><i class="bi bi-caret-up"></i></button>
         <p class="quantity_text_basket_product">${element.quantity}</p>
         <button class="quantity_minus" onclick=productMinus()><i class="bi bi-caret-down"></i></button>
-    </div>
+    // </div>
     // </div>
 }
 export default cardProduct;
